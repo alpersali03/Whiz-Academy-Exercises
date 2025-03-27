d68f26c272f909f336bd3d28c8fc716f1fefac1f
Sigma98 <alsalilee@gmail.com>
2025-03-27 23:02:20 +0200

    Person Class C#

---
+++ C#/Person/Person/PersonClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class PersonClass
+    {
+        private string name;
+        private int age;
+
+        public PersonClass(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age; 
+        } 
+        public string Name 
+        { 
+            get 
+            { 
+                return name; 
+            } 
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
---
+++ C#/Person/Person/Program.cs
@@ -0,0 +1,9 @@
+﻿using Person;
+
+//person.Name = "Alice";
+//person.Age = 25;
+//Console.WriteLine("Name: " + person.Name);
+//Console.WriteLine("Age: " + person.Age);
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+PersonClass person = new PersonClass(name, age);
