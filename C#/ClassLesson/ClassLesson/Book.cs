6d470a3c2868fed6bd533d56ad1b4b4aba2a371f
Sigma98 <alsalilee@gmail.com>
2025-03-27 22:56:39 +0200

    Classes first lesson C#

---
+++ C#/ClassLesson/ClassLesson/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLesson
+{
+    public class Book
+    {
+        private int year;
+        private string author;
+        private string title;
+        public string Title
+        {
+
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+    }
+}
---
+++ C#/ClassLesson/ClassLesson/Program.cs
@@ -0,0 +1,11 @@
+﻿using ClassLesson;
+using System.Net;
+
+Book book = new Book();
+book.Author = "Erik von Markovik";
+book.Title = "Mystery Method";
+book.Year = 2006;
+
+Console.WriteLine("Author: " + book.Author);
+Console.WriteLine("Title: " + book.Title);
+Console.WriteLine("Year: " + book.Year);
