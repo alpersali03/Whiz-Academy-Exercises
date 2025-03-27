ea17dfdbb204387b194a137a093ba54278890c35
Sigma98 <alsalilee@gmail.com>
2025-03-27 22:58:01 +0200

    Class-homework C#

---
+++ C#/HomeworkClass/HomeworkClass/CarClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkClass
+{
+    public class CarClass
+    {
+        private string brand;
+        private string model;
+        private int year;
+
+
+        public CarClass(string model, string brand, int year){
+            this.Model = model;
+            this.Brand = brand;
+            this.Year = year;
+        }
+        public CarClass(int year)
+        {
+            this.Year = year;
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Brand value is null");
+                }
+                brand = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                if(value < 2000)
+                {
+                    throw new ArgumentException("The year of the car should be beyond 2000 year");
+                }
+                year = value;   
+            }
+        }
+        public void CarInfo()
+        {
+            Console.WriteLine("Our car brand is : " + this.Brand);
+            Console.WriteLine("Our car model is : " + this.Model);
+            Console.WriteLine("Our car year is : " + this.Year);
+        }
+    }
+}
---
+++ C#/HomeworkClass/HomeworkClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using HomeworkClass;
+
+CarClass car =  new CarClass("Ford", "Mustang", 2015);
+//car.Brand = "";
+//car.Model = "Mustang";
+//car.Year = 1978;
+//Console.WriteLine("Brand: " + car.Brand);
+//Console.WriteLine("Model: " + car.Model);
+//Console.WriteLine("Year: " + car.Year);
+car.CarInfo();
+
+
+CarClass porche = new CarClass(2500);
+porche.CarInfo();
