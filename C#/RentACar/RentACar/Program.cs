c221f0a64d89dd5f33f9dbfbdd1b415e3c21dfe2
Sigma98 <alsalilee@gmail.com>
2025-03-29 23:11:47 +0200

    RentACar class C#

---
+++ C#/RentACar/RentACar/Program.cs
@@ -0,0 +1,7 @@
+﻿using RentACar;
+
+RebtalCar car = new RebtalCar();
+car.RentCar();
+Console.WriteLine(car.IsRented ? "rented" : "free");
+car.ReturnCar();
+Console.WriteLine($"The condition of the car is: {car.IsRented}");
---
+++ C#/RentACar/RentACar/RebtalCar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    public class RebtalCar
+    {
+        private string carModel;
+        private bool isRented;
+
+        public RebtalCar()
+        {
+            this.IsRented = false;
+        }
+        public string CarModel
+        {
+            get
+            {
+                return carModel;
+            }
+            set
+            {
+                carModel = value;
+            }
+        }
+        public bool IsRented
+        {
+            get
+            {
+                return isRented;
+            }
+            set
+            {
+                isRented = value;
+            }
+        }
+        public void RentCar()
+        {
+            IsRented = true;
+        }
+        public void ReturnCar()
+        {
+            IsRented = false;
+        }
+    }
+}
