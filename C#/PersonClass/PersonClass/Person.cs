8c7cd7370a1e4cb965ec897172b604b19bd17355
Sigma98 <alsalilee@gmail.com>
2025-03-29 23:16:10 +0200

    PersonClass C#

---
+++ C#/PersonClass/PersonClass/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    public class Person
+    {
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return Age;
+            }
+            set
+            {
+                Age = value;
+            }
+        }
+
+        public Person(string name, int age) 
+        {
+            this.Name = name;
+            this.Age = age; 
+        }
+       
+    }
+}
