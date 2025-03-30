dd62ad556441d9b17b42f22bc8ec8a8c0f22a145
Sigma98 <alsalilee@gmail.com>
2025-03-30 23:11:45 +0300

    RecordingMethod C#

---
+++ C#/RecordingMethod/RecordingMethod/Program.cs
@@ -0,0 +1,6 @@
+﻿using RecordingMethod;
+
+Dog dog = new Dog();
+Pussycat pussycat = new Pussycat();
+dog.MakeSound();
+pussycat.MakeSound();
---
+++ C#/RecordingMethod/RecordingMethod/Pussycat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingMethod
+{
+    public class Pussycat:Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+
+    }
+}
