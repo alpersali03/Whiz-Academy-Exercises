a1a56ad26d0151db549599421506501c9ed3b338
Sigma98 <alsalilee@gmail.com>
2025-03-31 23:09:59 +0300

    Books C#

---
+++ C#/Books/Books/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public List<BorrowedBooks> books { get; set; } = new List<BoBorrowedBooks>();
+    }
+}
