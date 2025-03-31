a5e928d85264306280c6d6c85f3f91a460f6a586
Sigma98 <alsalilee@gmail.com>
2025-03-31 23:10:18 +0300

    BrowserHistory

---
+++ C#/BrowserHistory/BrowserHistory/Program.cs
@@ -0,0 +1,26 @@
+﻿//Stack<string>websites = new Stack<string>();
+//string command = Console.ReadLine();
+//while(command != "2")
+//{
+//    // 1 githyb
+//    // 1 facebook 
+//    string[] cmd = command.Split(' ');
+//    websites.Push(cmd[1]);
+
+//    command = Console.ReadLine();
+//}
+//string lastWebsite = websites.Peek();
+//Console.WriteLine(lastWebsite);
+
+
+Stack<string> website = new Stack<string>();
+string command = Console.ReadLine();
+
+while (command != "2")
+{
+    string[] cmd = command.Split(" ");
+    website.Push(cmd[1]);
+    command = Console.ReadLine();
+}
+string lastWebsite = website.Peek();
+Console.WriteLine(lastWebsite);
