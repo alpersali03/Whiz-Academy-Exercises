3914351ac0ea7c835c2487c9532162754cba5460
Sigma98 <alsalilee@gmail.com>
2025-04-02 23:41:30 +0300

    DatingApp exercise C#

---
+++ C#/DatingApp/DatingApp/DatingProfile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp
+{
+    public class DatingProfile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Interests { get; set; }
+
+        public DatingProfile(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Interests = new List<string>();
+        }
+        public void AddInterest(string interest)
+        {
+            Interests.Add(interest);
+        
+        }
+        public void RemoveInterest(string interest)
+        {
+            if (Interests.Contains(interest))
+            {
+                Interests.Remove(interest);
+            }
+            else
+            {
+                throw new FileNotFoundException("Cannot find interest");
+            }
+        }
+        public bool Matches(DatingProfile otherProfile)
+        {
+            foreach(string interest in  Interests)
+            {
+                if (otherProfile.Interests.Contains(interest))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool IsEligibleForDating(int minAge, int maxAge)
+        {
+            if (Age >= minAge && Age <= maxAge)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string GetProfileSummary()
+        {
+            string interests = string.Join(" ", Interests);
+            return $"Name: {Name}\nAge: {Age}\nInterests: {interests}";
+        }
+    }
+}
---
+++ C#/DatingApp/DatingProfile.Test/DatingProfileTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Test
+{
+    public class DatingProfileTests
+    {
+        private DatingProfile profile;
+        [SetUp]
+        public void Setup()
+        {
+            string hobby = "Kickboxing";
+            this.profile = new DatingProfile("Alper Sali", 21);
+            this.profile.AddInterest(hobby);
+        }
+        [Test] 
+        public void AddInterestsShouldAddCorrectly()
+        {
+            //Arrange
+            string interest = "Dating";
+            int expectedInterestCount = 2;
+
+            //Act
+            profile.AddInterest(interest);
+            int interestCount = profile.Interests.Count();
+
+            //Assert
+            Assert.AreEqual(interestCount, expectedInterestCount, "Interest, added correctly");
+            
+        }
+        [Test]  
+        public void RemoveInterestsShouldRemoveCorrectly()
+        {
+            //Arrange
+            string interest = "Kickboxing";
+            int expectedInterestCount = 0;
+
+            //Act
+            profile.RemoveInterest(interest);
+            int interestCount = profile.Interests.Count();
+
+            //Assert
+            Assert.AreEqual(interestCount, expectedInterestCount, "Interest, removed correctly");
+
+        }
+        [Test]
+   public void RemoveInterestsShouldThrowExceptionNotFound()
+        {
+            //Arrange
+            string fakeInterest = "test";
+            string excpectionText = "Cannot find interest";
+            //Assert
+            var ex = Assert.Throws<FileNotFoundException>(() => profile.RemoveInterest(fakeInterest));
+            Assert.AreEqual(excpectionText, ex.Message);
+        }
+    }
+}
