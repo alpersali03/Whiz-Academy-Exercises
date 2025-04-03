1eee5e8f45c14e719d2e98e2bbc06b77018b5d76
Sigma98 <alsalilee@gmail.com>
2025-04-03 23:01:32 +0300

    Introduction to Entity Framework Core

---
+++ C#/EntityFrameworkLesson/EntityFrameworkLesson/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkLesson
+{
+    public class Animal
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+    }
+
+}
---
+++ C#/EntityFrameworkLesson/EntityFrameworkLesson/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkLesson
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-AUT50J3;Database=TestingCodeFirst;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Animal> Animals { get; set; }
+    }
+     
+}
