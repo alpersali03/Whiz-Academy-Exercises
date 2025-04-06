6474cf30ef36f4ab624b89c2ed19674b5c1c74fc
Sigma98 <alsalilee@gmail.com>
2025-04-06 23:21:52 +0300

    GameSimulation C#

---
+++ C#/GameSimulation/GameSimulation/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] players = new string[10];
+string turningNumber = players[3];
+Stack<string> stack = new Stack<string>();
+
+for (int i = 0; i < turningNumber.Length; i++)
+{
+    stack.Push(players[i]);
+    stack.Pop();
+    if (stack.Count == 1)
+    {
+        string winningPlayer = stack.Peek();
+        Console.WriteLine(winningPlayer);
+    }
+    
+}
