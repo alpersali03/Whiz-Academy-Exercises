56283184e0ff3cd4a86ad21226ac6b2a8e4ad5a7
Sigma98 <alsalilee@gmail.com>
2025-04-06 23:23:53 +0300

    Kickboxing C#

---
+++ C#/Kickboxing/Kickboxer.Test/FighterProfileTest.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Kickboxing.Test
+{
+    public class FighterProfileTest
+    {
+        private FighterProfile fighterProfile;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.fighterProfile = new FighterProfile("Alper Wick", "Lightweight");
+        }
+        [Test]
+
+        public void ShouldAddTecniqueCorrectly()
+        {
+            //Arrange
+            string technique = "Middle Kick";
+            int expectedTechniqueCount = 1;
+            //Act
+            fighterProfile.AddTechnique(technique);
+            int techniqueCount = fighterProfile.Techniques.Count();
+            //Assert
+            Assert.AreEqual(expectedTechniqueCount, techniqueCount, "Technique added correctly");
+
+
+        }
+        [Test]
+
+        public void ShouldRemoveTecniqueCorrectly()
+        {
+            //Arrange
+            string technique = "High Kick";
+            int expectedTechniqueCount = 0;
+            //Act
+            fighterProfile.RemoveTechnique(technique); // nqma tazi tehnika - nishto nqma da stnae
+            int removedTechnique = fighterProfile.Techniques.Count();
+            //Assert
+            Assert.AreEqual(expectedTechniqueCount, removedTechnique, "Technique was not changed");
+
+
+        }
+        [Test]
+
+        public void ShouldNotRemoveTechniqueCorrectly()
+        {
+            //Arrange
+            string technique = "Low kick";
+            int removedTechniqueCount = 0;
+            //Act
+            
+            fighterProfile.RemoveTechnique(technique);
+            int removedTechnique = fighterProfile.Techniques.Count();
+            //Assert
+            Assert.AreEqual(removedTechniqueCount, removedTechnique, "Technique removed correctly");
+
+
+        }
+        [Test]
+        public void ShouldCompareFightersCorrectly()
+        {
+            //Arrange
+            string firstTechnique = "High Kick";
+            string secondTechnique = "High Kick";
+            var firstKickBoxer = new FighterProfile("Alper", "Lightweight");
+            var secondKickBoxer = new FighterProfile("Ivan", "Lightweight");
+            firstKickBoxer.Techniques.Add(firstTechnique);
+            secondKickBoxer.Techniques.Add(secondTechnique);
+            bool expectedResult = true;
+            //Act
+            var comparison = firstKickBoxer.HasCommonTechnique(secondKickBoxer);
+            //Assert
+            Assert.AreEqual(expectedResult, comparison, "Technique was not changed");
+
+        }
+        [Test]
+        public void ShouldNotCompareFightersCorrectly()
+        {
+            //Arrange
+            string firstTechnique = "High Kick";
+            string secondTechnique = "Low Kick";
+            var firstKickBoxer = new FighterProfile("Alper", "Lightweight");
+            var secondKickBoxer = new FighterProfile("Ivan", "Lightweight");
+            firstKickBoxer.Techniques.Add(firstTechnique);
+            secondKickBoxer.Techniques.Add(secondTechnique);
+            bool expectedResult = false;
+            //Act
+            var comparison = firstKickBoxer.HasCommonTechnique(secondKickBoxer);
+            //Assert
+            Assert.AreEqual(expectedResult, comparison, "Technique was not changed");
+
+        }
+        [Test]
+
+        public void ShouldCheckWeightClassCorrectly()
+        {
+            //Arrange
+            var firstKickBoxer = new FighterProfile("Alper", "Light weight");
+            string weightClass = "Light weight";
+            bool checkingClass = true;
+            //Act
+
+            var checkingWeightClass = firstKickBoxer.IsEligibleForFight(weightClass);
+            
+            //Assert
+            Assert.AreEqual(checkingClass, checkingWeightClass, "Fighter can fight on this category");
+
+
+        }
+        [Test]
+        public void ShouldNotCheckWeightClassCorrectly()
+        {
+            //Arrange
+            var firstKickBoxer = new FighterProfile("Alper", "Light weight");
+            string weightClass = "Middle weight";
+            bool checkingClass = false;
+            //Act
+
+            var checkingWeightClass = firstKickBoxer.IsEligibleForFight(weightClass);
+
+            //Assert
+            Assert.AreEqual(checkingClass, checkingWeightClass, "Fighter can fight on this category");
+
+
+        }
+    }
+}
---
+++ C#/Kickboxing/Kickboxing/FighterProfile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kickboxing
+{
+    public class FighterProfile
+    {
+        public string Name { get; set; }
+        public string WeightClass { get; set; }
+        public List<string> Techniques { get; set; }
+
+        public FighterProfile(string name, string weightClass)
+        {
+            this.Name = name;
+            this.WeightClass = weightClass;
+            Techniques = new List<string>();
+        }
+        public void AddTechnique(string technique)
+        {
+            Techniques.Add(technique);
+        }
+
+        public void RemoveTechnique(string technique) 
+        { 
+            if (Techniques.Contains(technique))
+            {
+                Techniques.Remove(technique);
+            }
+            
+        }
+
+        public bool HasCommonTechnique(FighterProfile otherFighter) 
+        {
+            foreach (string technique in Techniques)
+            {
+                if (otherFighter.Techniques.Contains(technique))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool IsEligibleForFight(string requiredWeightClass)
+        {
+            if(WeightClass == requiredWeightClass)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string GetProfileSummary()
+        {
+            string interests = string.Join(" ", Techniques);
+            return $"Name: {Name}\nAge: {WeightClass}\nInterests: {Techniques}";
+        }
+    }
+    
+}
