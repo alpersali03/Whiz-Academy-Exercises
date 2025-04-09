cade9469f9619d425a45a7145b192fcf4d2660ec
Sigma98 <alsalilee@gmail.com>
2025-04-09 21:55:33 +0300

    MaxElement exercise C#

---
+++ C#/MaxElement/MaxElement/Program.cs
@@ -0,0 +1,44 @@
+﻿//Stack<int> stack = new Stack<int>();
+//int n = int.Parse(Console.ReadLine());
+
+//int maxNum = -1000;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int number = int.Parse(Console.ReadLine());
+//    stack.Push(number);
+     
+//}
+
+
+//for (int i = 0; i < n; i++)
+//{ 
+//    int currentNumber  = stack.Pop();
+//    if (currentNumber > maxNum)
+//    {
+//        maxNum = currentNumber;
+//    }
+//}
+//Console.WriteLine(maxNum);
+
+
+Stack<int> stack = new Stack<int>();    
+int n  = int.Parse(Console.ReadLine());
+
+int maxNum = -1000;
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    stack.Push(number);
+}
+
+for (int i = 0;i < n; i++)
+{
+    int currentNumber = stack.Pop();
+    if (currentNumber > maxNum)
+    {
+        maxNum = currentNumber; 
+    }
+}
+Console.WriteLine(maxNum);
