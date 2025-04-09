9b0544b1c315e8764e6af794daa947d210e3acb6
Sigma98 <alsalilee@gmail.com>
2025-04-09 22:16:41 +0300

    Unit testing exercise 2 C#

---
+++ C#/Nunit/Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+// using Nunit; // Reference the main project namespace.
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        // private Calculator _calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            // _calc = new Calculator();
+        }
+
+        // [Test]
+        // public void Add_ShouldReturnCorrectSum()
+        // {
+        //     // Arrange
+        //     int x = 5, y = 3;
+
+        //     // Act
+        //     int result = _calc.Add(x, y);
+
+        //     // Assert
+        //     Assert.AreEqual(8, result); // Validate the result is correct.
+        // }
+    }
+}
---
+++ C#/Nunit/Nunit/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;   
+        }
+        public int Subtract(int a , int b)
+        {
+            return a - b;
+        }
+    }
+}
