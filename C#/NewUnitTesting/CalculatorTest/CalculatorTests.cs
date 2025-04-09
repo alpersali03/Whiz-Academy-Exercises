240238a10b0e81b844b233e9da1d0400b22738ae
Sigma98 <alsalilee@gmail.com>
2025-04-09 22:16:06 +0300

    Unit testing exercise C#

---
+++ C#/NewUnitTesting/CalculatorTest/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using NewUnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{    
+    [TestFixture]
+
+    public class CalculatorTests
+    {
+            private Calc calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.calc = new Calc(); // Ensure Calc class is correctly referenced
+        }
+
+        [Test]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange
+            int first = 10;
+            int second = 20;
+            int expectedResult = 30;
+
+            // Act
+            int result = this.calc.Add(first, second);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult), "Addition result is incorrect.");
+
+        }
+    }
+}
---
+++ C#/NewUnitTesting/TestingClass/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace TestingClass
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
