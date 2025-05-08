376d89991202f7b81a6b4669f1e67a52b26f480d
Sigma98 <alsalilee@gmail.com>
2025-05-08 11:22:58 +0300

    ReverseSack exercise C#

---
+++ C#/ReverseSack/ReverseSack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+Stack<int> stack = new Stack<int>();
+int[] numbers = new int[5];
+
+for (int i = numbers.Length; i > 0; i--)
+{
+    stack.Push(i);
+   
+}
+int [] numbers2 = new int[numbers.Length];
+for (int i = 0;i < numbers2.Length; i++)
+{
+    numbers2[i] = stack.Pop();
+    
+}
+
+Console.WriteLine("[" + string.Join(", ",  numbers2) + "]");
