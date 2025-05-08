cb6774e9cb150a7a80ac523c1e0f7c74d3e84d76
Sigma98 <alsalilee@gmail.com>
2025-05-08 11:20:19 +0300

    PrinterQueue exercise C#

---
+++ C#/PrinterQueue/PrinterQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿Queue<(string Name, int Pages)> printerQueue = new Queue<(string, int)>();
+
+// Add documents to the queue
+printerQueue.Enqueue(("Документ1", 5));
+printerQueue.Enqueue(("Документ2", 3));
+
+// Process the printer queue
+while (printerQueue.Count > 0)
+{
+    // Dequeue the next document
+    var currentDocument = printerQueue.Dequeue();
+
+    // Simulate printing the document
+    Console.WriteLine($"Отпечатване на: {currentDocument.Name} ({currentDocument.Pages} страници)");
+    Console.WriteLine($"Завършено: {currentDocument.Name}");
+}
