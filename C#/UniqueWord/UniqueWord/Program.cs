fbd3110a6ac17b8aa4a1f592ee2a916a69f29b58
Sigma98 <alsalilee@gmail.com>
2025-06-10 23:24:05 +0300

    C# unique word

---
+++ C#/UniqueWord/UniqueWord/Program.cs
@@ -0,0 +1,26 @@
+﻿Dictionary<char, int> charCounts = new Dictionary<char, int>();
+string symbols = Console.ReadLine();
+for (int i = 0; i < symbols.Length; i++)
+{
+    char currentChar = symbols[i];
+    if (charCounts.ContainsKey(currentChar))
+    {
+        
+        charCounts[currentChar]++;
+    }
+    else
+    {
+        
+        charCounts[currentChar] = 1;
+    }
+
+}
+int misFound = charCounts.Values.Where(x => x > 1).FirstOrDefault(); 
+if(misFound == 0)
+{
+    Console.WriteLine("The symbols are unique");
+}
+else if (misFound > 1)
+{
+    Console.WriteLine("The symbols are not unique");
+}
