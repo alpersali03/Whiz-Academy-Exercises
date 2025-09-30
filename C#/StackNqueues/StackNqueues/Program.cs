6bde908db81e865a0eaf1d3aae1c1356c8467abb
Sigma98 <alsalilee@gmail.com>
2025-09-30 23:49:06 +0300

    StackNQueues

---
+++ C#/StackNqueues/StackNqueues/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections;
+using System.Net.Http.Headers;
+
+//Queue q = new Queue();
+//q.Enqueue(5);
+//Console.WriteLine(q.Peek());
+
+Stack<char> stack = new Stack<char>();
+string myStrings = Console.ReadLine();
+
+for (int i = 0; i < myStrings.Length; i++)
+{
+    char currentString = myStrings[i];
+    stack.Push(currentString);
+   
+}
+for (int i = 0;i < stack.Count; i++)
+{
+    Console.WriteLine(stack.Pop());
+    i--;
+}
+
