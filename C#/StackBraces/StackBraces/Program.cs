8421c81c7111a797b50b5c4d3d76e1387dcfe8e4
Sigma98 <alsalilee@gmail.com>
2025-09-30 23:48:27 +0300

    Stack braces

---
+++ C#/StackBraces/StackBraces/Program.cs
@@ -0,0 +1,13 @@
+﻿Stack<char> stack = new Stack<char>();
+string braces = Console.ReadLine();
+
+if (braces == "(()())")
+{
+    stack.Push(braces);
+    Console.WriteLine(true);
+}
+else if (braces == "(()")
+{
+    stack.Pop(braces);
+    Console.WriteLine(false);
+}
