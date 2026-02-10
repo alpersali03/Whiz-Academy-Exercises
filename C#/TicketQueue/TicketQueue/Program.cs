5dcfa1a574af1da52c58ce29aa9f41771c078649
Sigma98 <alsalilee@gmail.com>
2026-02-10 23:11:01 +0200

    TicketQueue exercise C#

---
+++ C#/TicketQueue/TicketQueue/Program.cs
@@ -0,0 +1,19 @@
+﻿Queue<string>ticketQueue = new Queue<string> { };
+
+ticketQueue.Enqueue("Alper");
+ticketQueue.Enqueue("Ivan");
+ticketQueue.Enqueue("Zdravko");
+ticketQueue.Enqueue("Dimitar");
+ticketQueue.Enqueue("Selim");
+ticketQueue.Enqueue("Alpay");
+
+foreach(string name in ticketQueue)
+{
+    Console.WriteLine($"Waiting on the queue - {name}");
+}
+
+while(ticketQueue.Count > 0)
+{
+    string servedPerson = ticketQueue.Dequeue();
+    Console.WriteLine($"Served - {servedPerson}");
+}
