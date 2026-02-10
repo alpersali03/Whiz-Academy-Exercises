8969b8a3a2fc1f97778acc7b44542224c1d84518
Sigma98 <alsalilee@gmail.com>
2026-02-11 00:14:06 +0200

    Tracking Amount exercise C#

---
+++ C#/TrackingAmount/TrackingAmount/Program.cs
@@ -0,0 +1,26 @@
+﻿Dictionary<string, int> product = new Dictionary<string, int>();
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    string[] cmds = command.Split(" ");
+    string productName = cmds[0];
+    int quantity = int.Parse(cmds[1]);
+    if (product.ContainsKey(productName))
+    {
+        product[productName] += quantity;
+
+    }
+    else
+    {
+        product[productName] = quantity;
+    }
+   
+    command = Console.ReadLine();
+    
+    
+}
+foreach(var item in product)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
