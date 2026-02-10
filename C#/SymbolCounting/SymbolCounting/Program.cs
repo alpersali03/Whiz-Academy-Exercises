4c74000c8eb6caf90c2219fa58f92c9d17e0b589
Sigma98 <alsalilee@gmail.com>
2026-02-10 23:03:49 +0200

    SymbolCounting exercise

---
+++ C#/SymbolCounting/SymbolCounting/Program.cs
@@ -0,0 +1,19 @@
+﻿Dictionary<char, int> text = new Dictionary<char, int>();
+string word = Console.ReadLine();
+for (int i = 0; i < word.Length; i++)
+{
+    char caracter = word[i];    
+
+    if (text.ContainsKey(caracter))
+    {
+        text[caracter]++;
+    }
+    else
+    {
+        text[caracter] = 1;
+    }
+}
+foreach (var entry in text)
+{
+    Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
