71971fd8501f1bd9d69363ea8926d4489c8ded18
Sigma98 <alsalilee@gmail.com>
2026-02-10 23:08:33 +0200

    Student exercise in C#

---
+++ C#/Students/Students/Program.cs
@@ -0,0 +1,14 @@
+﻿Dictionary<string, List<int>> students = new Dictionary<string, List<int>>();
+string command = Console.ReadLine();
+
+while (command != "stop")
+{
+    string name = Console.ReadLine();
+    List<int>notes = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+    students[name] = notes;
+    command = Console.ReadLine();
+}
+foreach (var student in students)
+{
+    
+}
