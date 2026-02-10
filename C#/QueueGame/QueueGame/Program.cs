6aee1de6c812f92979556c83d0ddf7bdd08c39dd
Sigma98 <alsalilee@gmail.com>
2026-02-10 22:59:33 +0200

    QueueGame exercise

---
+++ C#/QueueGame/QueueGame/Program.cs
@@ -0,0 +1,15 @@
+﻿List<string>players = new List<string> { "Ана", "Боби", "Катя", "Дани" };
+int rotation = 3;
+Queue<string> queue = new Queue<string>(players);
+
+while (queue.Count > 1)
+{
+    for (int i = 1; i < rotation - 2 ; i++)
+    {
+        string player = queue.Dequeue();
+        queue.Enqueue(player);
+    }
+    string removedPlayer = queue.Dequeue();
+}
+string winner = queue.Dequeue();
+Console.WriteLine(winner);
