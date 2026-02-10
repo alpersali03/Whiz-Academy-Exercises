351bdeba5f39a92f689792f33819d378f5a97d70
Sigma98 <alsalilee@gmail.com>
2026-02-10 23:02:14 +0200

    Synonim finding exercise

---
+++ C#/Synonims/Synonims/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+Dictionary<string, string> synonyms = new Dictionary<string, string>();
+string command = Console.ReadLine();
+
+while (command != "stop")
+{
+    string[] cmds = command.Split(" -> ");
+    string word = cmds[0];
+    string synonymword = cmds[1];
+    synonyms[word] = synonymword;  
+    command = Console.ReadLine();
+}
+string secondWord = Console.ReadLine();
+if (synonyms.ContainsKey(secondWord))
+{
+
+    Console.WriteLine(synonyms[secondWord]);
+}
+else
+{
+    Console.WriteLine("The synonym doesn't exsist");
+    Console.WriteLine(synonyms);
+}
