91eea7bdf502ef333cafb5f86486ac608c395606
Sigma98 <alsalilee@gmail.com>
2026-02-10 23:10:15 +0200

    StocksAndPrices exercise C#

---
+++ C#/StocksAndPrices/StocksAndPrices/Program.cs
@@ -0,0 +1,16 @@
+﻿Dictionary<string, double> product = new Dictionary<string, double>();
+string command = Console.ReadLine();
+
+while(command != "end")
+{
+    string[] parts = command.Split(' ');
+    string productName = parts[0];
+    double price = double.Parse(parts[1]);
+    product[productName] = price;
+    command = Console.ReadLine();
+}
+foreach (var item in product)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value:F2}");
+}
+Console.WriteLine("No more products");
